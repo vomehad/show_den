c172855a6447a284a1addcb737a8ebe9bcab0f6b
d.mozhaev <d.mozhaev@digital-sector.ru>
2022-10-03 17:04:20 +0300

    build

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import router from "./router";
 import axios from "axios";
 import store from "./store/index";
 
-console.log(process.env.VUE_APP_API_URL);
-// export const API_URL = 'http://localhost/api/v1/';
 export const API_URL = process.env.VUE_APP_API_URL;
 
 Vue.config.productionTip = false
